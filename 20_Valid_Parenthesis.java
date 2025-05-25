e343ae7ce54052e136ec78cc725806eecaba443e
YASH-GARG-1 <53114057+YASH-GARG-1@users.noreply.github.com>
2025-05-25 17:28:10 +0100

    first commit

---
+++ 20_Valid_Parenthesis.java
@@ -0,0 +1,40 @@
+class Solution {
+  public boolean isValid(String s) {
+
+      Stack<Character> st = new Stack<>();
+      for(int i = 0; i < s.length(); i++)
+      {
+          char ch = s.charAt(i);
+          if(st.isEmpty())
+          {
+              st.push(ch);
+              continue;
+          }
+          char topElement = st.pop();
+          if(topElement == '(' && ch == ')')
+          {
+              continue;
+          }
+          else if(topElement == '{' && ch == '}')
+          {
+              continue;
+          }
+          else if(topElement == '[' && ch == ']')
+          {
+              continue;
+          }
+          else
+          {
+              st.push(topElement);
+              st.push(ch);
+          }
+      }
+
+      if(st.size() == 0)
+      {
+          return true;
+      }
+      
+      return false;
+  }
+}
