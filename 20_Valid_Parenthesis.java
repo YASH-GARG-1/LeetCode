65e756cf3edb0938f7c6f7996efbdd967eb67416
YASH-GARG-1 <53114057+YASH-GARG-1@users.noreply.github.com>
2025-05-25 17:29:22 +0100

    20: Valid Parenthesis

--- 20_Valid_Parenthesis.java
+++ 20_Valid_Parenthesis.java
@@ -1,40 +1,32 @@
+import java.util.Stack;
+
 class Solution {
   public boolean isValid(String s) {
 
-      Stack<Character> st = new Stack<>();
-      for(int i = 0; i < s.length(); i++)
-      {
-          char ch = s.charAt(i);
-          if(st.isEmpty())
-          {
-              st.push(ch);
-              continue;
-          }
-          char topElement = st.pop();
-          if(topElement == '(' && ch == ')')
-          {
-              continue;
-          }
-          else if(topElement == '{' && ch == '}')
-          {
-              continue;
-          }
-          else if(topElement == '[' && ch == ']')
-          {
-              continue;
-          }
-          else
-          {
-              st.push(topElement);
-              st.push(ch);
-          }
+    Stack<Character> st = new Stack<>();
+    for (int i = 0; i < s.length(); i++) {
+      char ch = s.charAt(i);
+      if (st.isEmpty()) {
+        st.push(ch);
+        continue;
       }
+      char topElement = st.pop();
+      if (topElement == '(' && ch == ')') {
+        continue;
+      } else if (topElement == '{' && ch == '}') {
+        continue;
+      } else if (topElement == '[' && ch == ']') {
+        continue;
+      } else {
+        st.push(topElement);
+        st.push(ch);
+      }
+    }
 
-      if(st.size() == 0)
-      {
-          return true;
-      }
-      
-      return false;
+    if (st.size() == 0) {
+      return true;
+    }
+
+    return false;
   }
 }
